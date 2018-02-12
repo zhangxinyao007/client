821ce065bd6a8091ed5659c980a2362dc683f20b
zhangxinyao <zhangxinyao@zhangxinyaodeMacBook-Pro.local>
2018-02-12 17:30:15 +0800

    fix

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,16 +1,54 @@
 <template lang="pug">
   el-row.NavHeader
     el-col(:span="24")
-      div.grid-content.bg-purple-dark.Blue
+      div.content
+        div.left-content.left
+          div.logo-border
+            img(src="./../assets/logo.png" width="50px" height="50px")
+        h2.left.name.margin-left-1 {{name}}
+        div.right-content
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      name: '张欣尧'
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
+  height = 70px
+  logo-width = 50px
   .NavHeader
     width 100%
-    height 50px
+    height height
+    background-color #409EFF
+    .el-col
+      height height
+    .content
+      width 1200px
+      height height
+      margin 0 auto
+      .left-content
+        width logo-width
+        height logo-width
+        margin ( (height - logo-width) / 2 )
+        .logo-border
+          width logo-width
+          height logo-width
+          border 1px solid #fff
+          border-radius logo-width
+          img
+            width logo-width - 2
+            height logo-width -2
+            border-radius logo-width - 2
+            margin 1px
+      .name
+        color #fff
+        height height
+        line-height height
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import ElementUi from 'element-ui'
 import Index from '@/views/Index'
 import '@/styles/common.styl'
 
 Vue.use(Router)
-Vue.use(ElementUi)
 
 export default new Router({
   routes: [
